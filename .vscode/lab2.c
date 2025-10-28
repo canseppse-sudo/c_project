77019db345a52253cf482e441d81a9a54efcb915
canseppse-sudo <canseppse@gmail.com>
2025-10-28 17:26:24 +0700

    first commit

---
+++ .vscode/lab2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+    int age=16;
+    char name[50]="kanyaaphat jaturaphiphittapornchai";
+    char gender ='f';
+    float gap=3.13;
+
+    printf("MY name %s \n", name); //แสดงข้อความ string
+    printf("I'm %d \n", age); // เลขจำนวนเต็ฒ %d Decimal
+    printf("Grade %f \n", gap); //เลขทศนิยม %f float,
+    printf("Gender %c \n", gender); // %c = char
+    return  0;
+
+}
---
+++ lab1.c
@@ -0,0 +1,7 @@
+#include "stdio.h"
+void main(void){
+    printf("Hello World");
+    printf("kanyaphat jaturaphiphittapornchai");
+
+
+}
